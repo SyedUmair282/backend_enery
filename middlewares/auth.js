42149778c999c9f7425cff9da6affa498fcd470a
SyedUmair282 <64644024+SyedUmair282@users.noreply.github.com>
2023-02-18 18:59:04 +0500

    commit for check

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
+const SECRET_KEY="AUTHORIZATION"
 const auth = async (req, res, next) => {
   try {
     let token = req.headers.authorization;    
     if (token) {
       token = token.split(" ")[1];
-      let user = jwt.verify(token, process.env.SECRET_KEY);
+      let user = jwt.verify(token, SECRET_KEY);
       req.userid = user.id;
     } else {
       res.status(401).json({ message: "unauthorized user" });
